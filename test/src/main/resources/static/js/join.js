bb55c91fe10e10a3d458f0ef8db0eedc94ff9db6
camael407 <camael407@naver.com>
2022-12-23 12:12:12 +0900

    회원가입시 미래 날짜 선택 불가 유효성 검사

--- test/src/main/resources/static/js/join.js
+++ test/src/main/resources/static/js/join.js
@@ -1,7 +1,7 @@
 /**
  * 
  */
- 
+
 // function validateForm(x) {
 //	if($(x).val() == ""){
 //		$(x).removeClass("is-valid")
@@ -16,10 +16,17 @@
 //		return true;
 //	}	
 //}
- 
- //회원가입 버튼
- $("#JoinBtn").click(function(){
-	if($("#username").val() == ""){
+
+//회원가입 버튼
+$("#JoinBtn").click(function() {
+	const date = new Date();
+
+	const year = date.getFullYear();
+	const month = ('0' + (date.getMonth() + 1)).slice(-2);
+	const day = ('0' + date.getDate()).slice(-2);
+	const dateStr = year + '-' + month + '-' + day;
+
+	if ($("#username").val() == "") {
 		$("#username").removeClass("is-valid")
 		$("#username").addClass("is-invalid");
 		$("#username").attr("required");
@@ -29,8 +36,8 @@
 		$("#username").removeClass("is-invalid")
 		$("#username").addClass("is-valid");
 		$("#username").removeAttr("required");
-	}	
-	if($("#realname").val() == ""){
+	}
+	if ($("#realname").val() == "") {
 		$("#realname").removeClass("is-valid")
 		$("#realname").addClass("is-invalid");
 		$("#realname").attr("required");
@@ -40,8 +47,8 @@
 		$("#realname").removeClass("is-invalid")
 		$("#realname").addClass("is-valid");
 		$("#realname").removeAttr("required");
-	}	
-	if($("#useremail").val() == ""){
+	}
+	if ($("#useremail").val() == "") {
 		$("#useremail").removeClass("is-valid")
 		$("#useremail").addClass("is-invalid");
 		$("#useremail").attr("required");
@@ -56,7 +63,7 @@
 		$("#useremail").removeAttr("required");
 		$("#server").removeAttr("required");
 	}
-	if($("#server").val() == ""){
+	if ($("#server").val() == "") {
 		$("#server").removeClass("is-valid")
 		$("#server").addClass("is-invalid");
 		$("#server").attr("required");
@@ -71,7 +78,7 @@
 		$("#useremail").removeClass("is-invalid")
 		$("#useremail").removeAttr("required");
 	}
-	if($("#password").val() == ""){
+	if ($("#password").val() == "") {
 		$("#password").removeClass("is-valid")
 		$("#password").addClass("is-invalid");
 		$("#password").attr("required");
@@ -82,7 +89,7 @@
 		$("#password").addClass("is-valid");
 		$("#password").removeAttr("required");
 	}
-	if($("#ConfPassword").val() == ""){
+	if ($("#ConfPassword").val() == "") {
 		$("#ConfPassword").removeClass("is-valid")
 		$("#ConfPassword").addClass("is-invalid");
 		$("#ConfPassword").attr("required");
@@ -105,18 +112,25 @@
 		$("#ConfPwd-wrong").css("display", "none");
 		$("#ConfPassword").removeAttr("required");
 	}
-	if($("#birth").val() == ""){
+	if ($("#birth").val() == "") {
 		$("#birth").removeClass("is-valid")
 		$("#birth").addClass("is-invalid");
 		$("#birth").attr("required");
 		$("#birth").focus()
 		return false;
+	} else if ($("#birth").val() > dateStr) {
+		alert("당신은 태어나지 않았습니까?")
+		return false;
+
 	} else {
 		$("#birth").removeClass("is-invalid")
 		$("#birth").addClass("is-valid");
 		$("#birth").removeAttr("required");
+		console.log("birth : " + $("#birth").val())
+		console.log("now : " + date)
+
 	}
-	if($("#age").val() == ""){
+	if ($("#age").val() == "") {
 		$("#age").removeClass("is-valid")
 		$("#age").addClass("is-invalid");
 		$("#age").attr("required");
@@ -127,7 +141,7 @@
 		$("#age").addClass("is-valid");
 		$("#age").removeAttr("required");
 	}
-	if($("#phone").val() == ""){
+	if ($("#phone").val() == "") {
 		$("#phone").removeClass("is-valid")
 		$("#phone").addClass("is-invalid");
 		$("#phone").attr("required");
@@ -138,7 +152,7 @@
 		$("#phone").addClass("is-valid");
 		$("#phone").removeAttr("required");
 	}
-	if($("#zipcode").val() == ""){
+	if ($("#zipcode").val() == "") {
 		$("#zipcode").removeClass("is-valid")
 		$("#zipcode").addClass("is-invalid");
 		$("#zipcode").attr("required");
@@ -149,7 +163,7 @@
 		$("#zipcode").addClass("is-valid");
 		$("#zipcode").removeAttr("required");
 	}
-	if($("#address1").val() == ""){
+	if ($("#address1").val() == "") {
 		$("#address1").removeClass("is-valid")
 		$("#address1").addClass("is-invalid");
 		$("#address1").attr("required");
@@ -160,90 +174,90 @@
 		$("#address1").addClass("is-valid");
 		$("#address1").removeAttr("required");
 	}
-	if($("input[name=gender]:radio:checked").length < 1) {
+	if ($("input[name=gender]:radio:checked").length < 1) {
 		alert("성별을 선택해주세요")
 		return false;
 	}
 	//유효성 검사
-	
+
 	//controller 콜
 	addr = {
-			zipcode : $("#zipcode").val(),
-			address1 : $("#address1").val(),
-			address2 : $("#address2").val()
+		zipcode: $("#zipcode").val(),
+		address1: $("#address1").val(),
+		address2: $("#address2").val()
 	}
-	
+
 	data = {
-			phone : $("#phone").val(),
-			age : $("#age").val(),
-			birth : $("#birth").val(),
-			useremail : $("#useremail").val()+"@"+$("#server").val(),
-			realname : $("#realname").val(),
-			password : $("#password").val(),
-			username : $("#username").val(),
-			gender : $("input[name=gender]:radio:checked").val(),
-			address : addr			
+		phone: $("#phone").val(),
+		age: $("#age").val(),
+		birth: $("#birth").val(),
+		useremail: $("#useremail").val() + "@" + $("#server").val(),
+		realname: $("#realname").val(),
+		password: $("#password").val(),
+		username: $("#username").val(),
+		gender: $("input[name=gender]:radio:checked").val(),
+		address: addr
 	}
-	
+
 	$.ajax({
-		type : "post",
-		url : "/join",
-		contentType : "application/json",
-		data : JSON.stringify(data)
+		type: "post",
+		url: "/join",
+		contentType: "application/json",
+		data: JSON.stringify(data)
 	})//ajax
-	.done(function(resp){
-		if(resp == "success"){
-			location.href = "/"
-		} else{
-			alert("아이디가 중복됩니다.")
-		}
-	})//done
+		.done(function(resp) {
+			if (resp == "success") {
+				location.href = "/"
+			} else {
+				alert("아이디가 중복됩니다.")
+			}
+		})//done
 })//JoinBtn
 
-	//addressAPI
-    //본 예제에서는 도로명 주소 표기 방식에 대한 법령에 따라, 내려오는 데이터를 조합하여 올바른 주소를 구성하는 방법을 설명합니다.
-    function sample4_execDaumPostcode() {
-        new daum.Postcode({
-            oncomplete: function(data) {
-                // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+//addressAPI
+//본 예제에서는 도로명 주소 표기 방식에 대한 법령에 따라, 내려오는 데이터를 조합하여 올바른 주소를 구성하는 방법을 설명합니다.
+function sample4_execDaumPostcode() {
+	new daum.Postcode({
+		oncomplete: function(data) {
+			// 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
 
-                // 도로명 주소의 노출 규칙에 따라 주소를 표시한다.
-                // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
-                var roadAddr = data.roadAddress; // 도로명 주소 변수
-                var extraRoadAddr = ''; // 참고 항목 변수
+			// 도로명 주소의 노출 규칙에 따라 주소를 표시한다.
+			// 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+			var roadAddr = data.roadAddress; // 도로명 주소 변수
+			var extraRoadAddr = ''; // 참고 항목 변수
 
-                // 법정동명이 있을 경우 추가한다. (법정리는 제외)
-                // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
-                if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
-                    extraRoadAddr += data.bname;
-                }
-                // 건물명이 있고, 공동주택일 경우 추가한다.
-                if(data.buildingName !== '' && data.apartment === 'Y'){
-                   extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
-                }
+			// 법정동명이 있을 경우 추가한다. (법정리는 제외)
+			// 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+			if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
+				extraRoadAddr += data.bname;
+			}
+			// 건물명이 있고, 공동주택일 경우 추가한다.
+			if (data.buildingName !== '' && data.apartment === 'Y') {
+				extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+			}
 
-                // 우편번호와 주소 정보를 해당 필드에 넣는다.
-                document.getElementById('zipcode').value = data.zonecode;
-                document.getElementById("address1").value = roadAddr;               
-                
-                var guideTextBox = document.getElementById("guide");
-                // 사용자가 '선택 안함'을 클릭한 경우, 예상 주소라는 표시를 해준다.
-                if(data.autoRoadAddress) {
-                    var expRoadAddr = data.autoRoadAddress + extraRoadAddr;
-                    guideTextBox.innerHTML = '(예상 도로명 주소 : ' + expRoadAddr + ')';
-                    guideTextBox.style.display = 'block';
-                    guideTextBox.parentNode.style.display = 'block';
+			// 우편번호와 주소 정보를 해당 필드에 넣는다.
+			document.getElementById('zipcode').value = data.zonecode;
+			document.getElementById("address1").value = roadAddr;
 
-                } else if(data.autoJibunAddress) {
-                    var expJibunAddr = data.autoJibunAddress;
-                    guideTextBox.innerHTML = '(예상 지번 주소 : ' + expJibunAddr + ')';
-                    guideTextBox.style.display = 'block';
-                    guideTextBox.parentNode.style.display = 'block';
-                } else {
-                    guideTextBox.innerHTML = '';
-                    guideTextBox.style.display = 'none';
-                    guideTextBox.parentNode.style.display = 'none';
-                }
-            }
-        }).open();      
-    }
+			var guideTextBox = document.getElementById("guide");
+			// 사용자가 '선택 안함'을 클릭한 경우, 예상 주소라는 표시를 해준다.
+			if (data.autoRoadAddress) {
+				var expRoadAddr = data.autoRoadAddress + extraRoadAddr;
+				guideTextBox.innerHTML = '(예상 도로명 주소 : ' + expRoadAddr + ')';
+				guideTextBox.style.display = 'block';
+				guideTextBox.parentNode.style.display = 'block';
+
+			} else if (data.autoJibunAddress) {
+				var expJibunAddr = data.autoJibunAddress;
+				guideTextBox.innerHTML = '(예상 지번 주소 : ' + expJibunAddr + ')';
+				guideTextBox.style.display = 'block';
+				guideTextBox.parentNode.style.display = 'block';
+			} else {
+				guideTextBox.innerHTML = '';
+				guideTextBox.style.display = 'none';
+				guideTextBox.parentNode.style.display = 'none';
+			}
+		}
+	}).open();
+}
